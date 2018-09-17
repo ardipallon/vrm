da1bfbbf29582958bdf87f26d9e4734cd559a713
Ardi Pallon <ardi.pallon@khk.ee>
2018-09-17 14:46:14 +0300

    git add .! REDO FROM Separate Application.php logic into more different methods git add .!

--- src/Application.php
+++ src/Application.php
@@ -1,15 +1,8 @@
 <?php
-
 namespace BookingApp;
-
-use BookingApp\Controllers\CreateBookingController;
 use Silex\Application as SilexApplication;
 use Silex\Provider\DoctrineServiceProvider;
-use Silex\Provider\FormServiceProvider;
-use Silex\Provider\LocaleServiceProvider;
-use Silex\Provider\TranslationServiceProvider;
 use Silex\Provider\TwigServiceProvider;
-
 /**
  * Custom Application class that hold our application specifix functionality.
  */
@@ -18,24 +11,19 @@ class Application extends SilexApplication
     public function __construct(array $values = [])
     {
         parent::__construct($values);
-
         $this->configureServices();
         $this->createDBTables();
         $this->configureControllers();
     }
-
     /**
      * Config app options and register services.
      */
     private function configureServices()
     {
-
         $this['debug'] = true;
-
         $this->register(new TwigServiceProvider(), [
             'twig.path' => __DIR__.'/../views',
         ]);
-
         // Database configuration
         $this->register(new DoctrineServiceProvider(), [
             'db.options' => [
@@ -43,19 +31,12 @@ class Application extends SilexApplication
                 'path' => __DIR__.'/../database/app.db',
             ],
         ]);
-
-        $this->register(new FormServiceProvider());
-        $this->register(new LocaleServiceProvider());
-        $this->register(new TranslationServiceProvider(), [
-            'translator.domains' => [],
-        ]);
     }
         /**
      * Creates all needed tables to database if they don't exist.
      */
     private function createDBTables()
     {
-
         if (!$this['db']->getSchemaManager()->tablesExist('bookings')) {
             $this['db']->executeQuery("CREATE TABLE bookings (
                 id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -73,19 +54,13 @@ class Application extends SilexApplication
             );");
         }
     }
-
     /**
      * Define all used routes and connect a route to its controller.
      */
     private function configureControllers()
     {
-        $this
-            ->match('/bookings/create', new CreateBookingController(
-                $this['form.factory'],
-                $this['twig'],
-                $this['db']
-            ))
-            ->method('GET|POST')
-        ;
+        $this->get('/bookings/create', function () {
+            return $this['twig']->render('base.html.twig');
+        });
     }
 }
